3c80ff683546978e7d0e2693b082ca5a8ce831bc
Codingmasterfred <fredrickg777@gmail.com>
2023-02-09 08:15:48 -0800

    update

---
+++ script.js
@@ -0,0 +1,40 @@
+
+window.onload = function() {
+    whodat();
+    //run js code here
+}
+
+
+let song = '';
+
+function whodat() {
+    song = prompt('Who is your favorite artist?');
+    if(song != 'Nba youngboy' ){
+        alert('Wrong Answer!!😈');
+
+    }else {
+        alert('Mines too ;) ');
+    }
+
+}
+
+
+function getTextareaValue(){
+    var textarea = document.getElementById('textarea');
+    var value = textarea.value;
+    alert('Thanks for your feedback.');
+
+};
+
+
+
+var mysong = new Audio ();
+mysong.src = 'song.mp3';
+
+const playsong = () =>{
+    mysong.play();
+}
+
+const pausesong = () =>{
+    mysong.pause();
+}
